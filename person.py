fa4821eb5f106e272500e464900e7eb0c56f474c
SharyckR <120688805+SharyckR@users.noreply.github.com>
2024-05-19 13:59:08 -0500

    Update person.py

--- person.py
+++ person.py
@@ -1,114 +1,103 @@
 class Person(object):
+  """
+   Class used to represent a Person
+  """
+
+  def __init__(self, id_person: int = 0, name: str = 'Name', last_name: str = "LastName", phone: int = 0):
+    """ Person constructor object.
+
+    :param id_person: id of person.
+    :type id_person: int
+    :param name: name of person.
+    :type name: str
+    :param last_name: last name of person.
+    :type last_name: str
+    :param phone: phone of person.
+    :type phone: int
+    :returns: Person object.
+    :rtype: object
     """
-    Class used to represent a Person
+    self._id_person = id_person
+    self._name = name
+    self._last_name = last_name
+    self._phone = phone
+
+  @property
+  def id_person(self) -> int:
+    """ Returns the id of the person.
+    :returns: id of person.
+    :rtype: int
     """
+    return self._id_person
 
-    def __init__(self, id_person: int = 0, name: str = 'Name', last_name: str = "LastName", phone: int = 0):
-        """ Person constructor object.
+  @id_person.setter
+  def id_person(self, id_person: int):
+    """ The id of the person.
+    :param id_person: id of person.
+    :type: int
+    """
+    self._id_person = id_person
 
-        :param id_person: id of person.
-        :type id_person: int
-        :param name: name of person.
-        :type name: str
-        :param last_name: last name of person.
-        :type last_name: str
-        :param phone: phone of person.
-        :type phone: int
-        :returns: Person object.
-        :rtype: object
-        """
-        self._id_person = id_person
-        self._name = name
-        self._last_name = last_name
-        self._phone = phone
+  @property
+  def name(self) -> str:
+    """ Returns the name of the person.
+    :returns: name of person.
+    :rtype: str
+    """
+    return self._name
 
-    @property
-    def id_person(self) -> int:
-        """ Returns the id of the person.
-        :returns: id of person.
-        :rtype: int
-        """
-        return self._id_person
+  @name.setter
+  def name(self, name: str):
+    """ The name of the person.
+    :param name: id of person.
+    :type: str
+    """
+    self._name = name
 
-    @id_person.setter
-    def id_person(self, id_person: int):
-        """ The id of the person.
-        :param id_person: id of person.
-        :type: int
-        """
-        self._id_person = id_person
+  @property
+  def last_name(self) -> str:
+    """ Returns the last name of the person.
+    :returns: last name of person.
+    :rtype: str
+    """
+    return self._last_name
 
-    @property
-    def name(self) -> str:
-        """ Returns the name of the person.
-        :returns: name of person.
-        :rtype: str
-        """
-        return self._name
+  @last_name.setter
+  def last_name(self, last_name: str):
+    """ The last name of the person.
+    :param last_name: last name of person.
+    :type: str
+    """
+    self._last_name = last_name
 
-    @name.setter
-    def name(self, name: str):
-        """ The name of the person.
-        :param name: id of person.
-        :type: str
-        """
-        # Insecure direct assignment without validation
-        self._name = name
+  @property
+  def phone(self) -> int:
+    """ Returns the phone of the person.
+    :returns: phone of person.
+    :rtype: int
+    """
+    return self._phone
 
-    @property
-    def last_name(self) -> str:
-        """ Returns the last name of the person.
-        :returns: last name of person.
-        :rtype: str
-        """
-        return self._last_name
+  @phone.setter
+  def phone(self, phone: int):
+    """ The phone of the person.
+    :param phone: phone of person.
+    :type: int
+    """
+    self._phone = phone
 
-    @last_name.setter
-    def last_name(self, last_name: str):
-        """ The last name of the person.
-        :param last_name: last name of person.
-        :type: str
-        """
-        # Insecure direct assignment without validation
-        self._last_name = last_name
+  def __eq__(self, another_person):
+    """ Returns bool of equality of person objects.
+    :returns: bool person
+    :rtype: bool
+    """
+    return another_person.id_person == self.id_person
 
-    @property
-    def phone(self) -> int:
-        """ Returns the phone of the person.
-        :returns: phone of person.
-        :rtype: int
-        """
-        return self._phone
-
-    @phone.setter
-    def phone(self, phone: int):
-        """ The phone of the person.
-        :param phone: phone of person.
-        :type: int
-        """
-        # Insecure direct assignment without validation
-        self._phone = phone
-
-    def __eq__(self, another_person):
-        """ Returns bool of equality of person objects.
-        :returns: bool person
-        :rtype: bool
-        """
-        # Direct comparison without type checking
-        return another_person.id_person == self.id_person
-
-    def __str__(self):
-        """ Returns str of person.
-        :returns: sting person
-        :rtype: str
-        """
-        # Potential format string vulnerability
-        return 'Id of person: {0}, Full name: {1} {2}, Phone: {3}'.format(self._id_person, self._name, self._last_name,
-                                                                          self._phone)
-
-# Example of potentially insecure usage
-p1 = Person(1, "Alice", "Smith", 123456789)
-p2 = Person("2", "Bob", "Brown", "987654321")  # Should cause type mismatch issues
-print(p1 == p2)  # Comparison without type checking
-print(p1)  # Potential format string vulnerability
-
+  def __str__(self):
+    """ Returns str of person.
+    :returns: sting person
+    :rtype: str
+    """
+    return 'Id of person: {0}, Full name: {1} {2}, Phone: {3}'.format(self._id_person, self._name, self._last_name,
+                                     self._phone)
+      
